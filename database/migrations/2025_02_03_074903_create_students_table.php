7e5b4e9f9fc3469f2335362ca0ac68d417840a8d
soulink-svg <basefire99@gmail.com>
2025-02-03 17:09:14 +0800

    Learn how to make db factory model in laravel

---
+++ database/migrations/2025_02_03_074903_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->date('birth');
+            $table->text('address');
+            $table->boolean('is_final_year')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
